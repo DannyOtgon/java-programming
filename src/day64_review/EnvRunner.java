88021c9c6854c463bc31cbd385d92f7379f97348
DannyOtgon <dannyotgon@gmail.com>
2021-09-15 10:04:42 -0400

    day63-finished (Collections )LAST DAY OF JAVA

---
+++ src/day64_review/EnvRunner.java
@@ -0,0 +1,9 @@
+package day64_review;
+
+public class EnvRunner {
+    public static void main(String[] args) {
+
+        System.out.println(Environment.QA);
+
+    }
+}
---
+++ src/day64_review/Environment.java
@@ -0,0 +1,8 @@
+package day64_review;
+
+public enum Environment {
+
+    DEV, QA, STAGING, PROD
+
+
+}
